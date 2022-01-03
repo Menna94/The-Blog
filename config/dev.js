41f866b8adf485a892882dab54a9b033c29570f3
Menna94 <menna.mooc94@gmail.com>
2022-01-03 23:55:11 +0200

    image-upload

--- config/dev.js
+++ config/dev.js
@@ -3,5 +3,7 @@ module.exports = {
   googleClientID:'121705036225-pci489qi7hifrr0do1udu8u3skpm176m.apps.googleusercontent.com',
   googleClientSecret: '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',
   cookieKey: '123123123',
-  redisUrl :'redis://127.0.0.1:6379'
+  redisUrl :'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIA54QBQBNMZJBEZ3TC',
+  secretAccessKey:'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'
 };
