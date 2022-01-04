77e010c494f7935903a04d615dc073aef56ddc58
Menna94 <menna.mooc94@gmail.com>
2022-01-04 02:31:26 +0200

    last commit

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,12 +1,33 @@
 //to generate presigned URL for image upload from AWS-Bucket
 const AWS = require('aws-sdk');
 const keys = require('../config/keys');
+const uuid = require('uuid/v1');
+const requireLogin = require('../middlewares/requireLogin')
 
 const s3 = new AWS.S3({
     accessKeyId: keys.accessKeyId,
     secretAccessKey: keys.secretAccessKey
-})
+    },
+    {
+        region: 'us-east-1'
+
+    }
+)
 
 module.exports = app =>{
-    app.get('/api/upload', (req,res)=>{})
+    app.get('/api/upload', requireLogin, (req,res)=>{
+        const uniqueKey = `${req.user.id}/${uuid()}.jpeg`;
+
+        s3.getSignedUrl('putObject', {
+            Bucket: 'the-blog-bucketer',
+            ContentType: 'image/jpeg',
+            //<UserId>/<randomStringOfCharactersAndImages>.<fileExtension>
+            Key: uniqueKey
+        }, (err, url)=>{
+            res.send({
+                uniqueKey,
+                url
+            })
+        } )
+    })
 }
